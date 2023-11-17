a7db0c242cba08b4e7da6b8b24b066ff02fe6e09
Prajil K <prajilk20017@gmail.com>
2023-11-17 14:26:55 +0530

    edit count made responsive

--- src/controllers/CountsController.js
+++ src/controllers/CountsController.js
@@ -34,7 +34,6 @@ module.exports = {
                 }
                 resolve(response);
             } catch (error) {
-                console.log(error);
                 reject()
             }
         })
